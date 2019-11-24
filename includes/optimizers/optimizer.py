7fbbc587357d78249c1e00bb7213f4be1d94e9af
Gurpreet Singh <g.singh6343@gmail.com>
2019-11-23 19:01:31 -0500

    Added update_model functionality in default optimizers

--- includes/optimizers/optimizer.py
+++ includes/optimizers/optimizer.py
@@ -22,10 +22,21 @@ class DefaultWrapper:
     def __init__(self, model, optimizer, **kwargs):
         self.model = model
         self.loss_fn = model.loss_fn
+
+        self.optimizer_fn = optimizer
         self.optimizer = optimizer(model.parameters(), **kwargs)
 
+        if "lr" in kwargs:
+            del kwargs["lr"]
+        self.optimizer_args = kwargs
+
     def update_model(self, model):
-        raise NotImplementedError
+        self.model = model
+        self.optimizer = self.optimizer_fn(
+            model.parameters(),
+            lr=self.optimizer.param_groups[0]["lr"],
+            **self.optimizer_args
+        )
 
     def step(self, data, device="cpu"):
         data, target = data[0].to(device), data[1].to(device)
